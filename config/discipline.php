616a8bb8dfcccdbe7028171d83190f092d419db7
expldec <explosive.deconstruction@gmail.com>
2022-07-18 10:22:14 +0200

    created models

---
+++ config/discipline.php
@@ -0,0 +1,98 @@
+<?php
+return [
+    "categories" => [
+        [
+            "name" =>  "100m stile rana",
+            "discipline" => "Nuoto",
+            "genre" => "Maschile"
+        ],
+        [
+            "name" =>  "200m stile libero",
+            "discipline" => "Nuoto",
+            "genre" => "Femminile"
+        ],
+        [
+            "name" =>  "Trampolino",
+            "discipline" => "Ginnastica",
+            "genre" => "Femminile"
+        ],
+        [
+            "name" => "Anelli",
+            "discipline" => "Ginnastica",
+            "genre" => "Maschile"
+        ],
+        [
+            "name" => "Greco-romana",
+            "discipline" => "Lotta",
+            "genre" => "Maschile"
+        ],
+        [
+            "name" => "3000 metri siepi",
+            "discipline" => "Atletica Leggera",
+            "genre" => "Maschile"
+        ],
+        [
+            "name" => "100 metri ostacoli",
+            "discipline" => "Atletica Leggera",
+            "genre" => "Femminile"
+        ],
+        [
+            "name" => "Lancio del disco",
+            "discipline" => "Atletica Leggera",
+            "genre" => "Femminile"
+        ]
+    ],
+    "countries" => [
+        [
+            "name" => "Italia",
+            "acronym" => "IT"
+        ],
+        [
+            "name" => "Stati Uniti d'America",
+            "acronym" => "USA"
+        ],
+        [
+            "name" => "Cina",
+            "acronym" => "CHN"
+        ],
+        [
+            "name" => "Regno Unito",
+            "acronym" => "GBR"
+        ],
+        [
+            "name" => "Germania",
+            "acronym" => "GER"
+        ],
+        [
+            "name" => "Marocco",
+            "acronym" => "MAR"
+        ],
+        [
+            "name" => "Cuba",
+            "acronym" => "CUB"
+        ],
+    ],
+    "athletes" => [
+        [
+            "name" => "Valarie Allman",
+            "genre" => "Donna"
+        ],
+        [
+            "name" => "Kendra Harrison",
+            "genre" => "Donna"
+        ],
+        [
+            "name" => "Soufiane el-Bakkali",
+            "genre" => "Uomo"
+        ],
+        [
+            "name" => "Luis Orta",
+            "genre" => "Uomo"
+        ],
+        [
+            "name" => "Nicolò Martinenghi",
+            "genre" => "Uomo"
+        ]
+    ]
+
+];
